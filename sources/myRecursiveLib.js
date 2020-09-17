599a43f713e278a25b6e77e72d94d8b063453998
Your Name <you@example.com>
2020-09-17 12:03:34 +0200

    tre rekursive løsninger

---
+++ sources/myRecursiveLib.js
@@ -0,0 +1,39 @@
+let fact = function(n){
+
+    if(n == 1){
+        return n;
+    }
+    if(n == 0){
+        return 1; 
+    }
+    
+    return n * fact(n-1); 
+}
+
+console.log(fact(5));
+
+let fibo = function(n){
+
+    if(n<=1){
+        return n;
+    }
+    return  fibo(n-1) + fibo(n-2);
+
+}
+
+console.log(fibo(7,7));
+
+let pow = function(r,e){
+     
+    if(e == 0){
+        return 1; 
+    }
+
+    if(e == 1){
+        return r; 
+    }
+
+    return r * pow(r,e-1);
+}
+
+console.log(pow(5,3));
